6799a46e9e109ab8253d4c8f2f2095508048d549
rowleks <momohrowland@gmail.com>
2025-01-27 10:27:53 +0100

    added template

---
+++ head-template.php
@@ -0,0 +1,6 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP | Tailwind</title>
+    <link rel="stylesheet" href="./styles/output.css">
+</head>
--- index.php
+++ index.php
@@ -5,13 +5,9 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./styles/output.css">
-</head>
+    <?php include './head-template.php' ?>
 <body>
+    
 
 </body>
 </html>
