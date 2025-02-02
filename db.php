556a56ee053f1f901cd71b9f9566426684b81e00
rowleks <momohrowland@gmail.com>
2025-02-02 15:07:52 +0100

    added api connection

--- db.php
+++ db.php
@@ -23,8 +23,22 @@
         global $conn;
         $q_string = "SELECT * FROM users";
         $result = $conn->query($q_string);
-        $data = $result->fetch_all(MYSQLI_ASSOC);
-        return $data;
+
+        if($result->num_rows)
+        {
+            $data = $result->fetch_all(MYSQLI_ASSOC);
+            return $data;
+        } 
+        else 
+        { 
+            echo "No data found"; 
+            return null;
+        }
+    }
+    function clear_db() {
+        global $conn;
+        $q_string = "DELETE FROM users";
+        $conn->query($q_string);
     }
 
     function insert_data($fn, $ln, $email) {
@@ -33,11 +47,14 @@
         {
             $q_stmt = $conn->prepare("INSERT INTO `users` (`firstname`, `lastname`, `email`) VALUES (?, ?, ?)");
             $q_stmt->bind_param("sss", $fn, $ln, $email);
+            $result = $q_stmt->get_result();
+            if($result->num_rows) 
+            { $data = $result->fetch_assoc(); }
 
             if($q_stmt->execute()) 
             { 
                 echo "Inserted data";
-                return $q_stmt; 
+                return $data;
             }
             else 
             { 
@@ -50,15 +67,39 @@
         { echo "Error: ". $e->getMessage(); }
     }
 
+    function update_data($name, $id )
+    {
+        global $conn;
+        try
+        {
+            $q_string = "UPDATE users SET firstname=? WHERE id=?";
+            $q_stmt = $conn->prepare($q_string);
+            $q_stmt->bind_param('sd', $name, $id);
+            if($q_stmt->execute()) { echo "updated data";}
+            else { echo "failed to update data";}
+
+        }
+        catch(Exception $e) { echo 'Error: '. $e;}
+
+    }
+
     connect_to_db();
-    /*     
-        insert_data('Rowland', 'Momoh', 'momohrowland@gmail.com');
-        insert_data('Sandra', 'Eleojo', 'sandraeleojo@gmail.com'); 
-    */
+
+/*  
+
+    insert_data('Rowland', 'Momoh', 'momohrowland@gmail.com');
+    insert_data('Shedrack', 'Baka', 'bakashedrack@gmail.com');
+    insert_data('Victor', 'Baka', 'bakavictor@gmail.com'); 
+*/
+    
+    update_data('Shedy', 10);
+    
     $info = query_db();
     $json = json_encode($info);
 
-    print_r($json);
+    echo '<br/> <br/>';
+
+    echo($json);
 
 
     $conn->close();
